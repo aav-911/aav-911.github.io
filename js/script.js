9f331180c88a31691b858206f449dc1e22271298
avega24 <avega24@georgefox.edu>
2026-05-18 11:11:47 -0700

    Finished homepage

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 const hamburger = document.getElementById("hamburger-menu");
-
 const navLinks = document.getElementById("nav-links");
 
 hamburger.addEventListener("click", () => {
-
-    navLinks.classList.toggle("active");
-
+  navLinks.classList.toggle("active");
 });
+
+const panels = document.querySelectorAll(".panel");
+const panelObserver = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
+panels.forEach((panel) => panelObserver.observe(panel));
